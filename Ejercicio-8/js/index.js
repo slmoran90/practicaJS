24078733a663cf27ffc582410a911cc57f4558eb
Sergio Moran <moransergio.01@gmail.com>
2020-12-11 01:10:31 -0300

    Ejercicio 8

---
+++ Ejercicio-8/js/index.js
@@ -0,0 +1,4 @@
+let num = parseInt(prompt('Número: '));
+
+if (num%2 == 0) document.write('El número ' + num + ' es divisible por 2.');
+else document.write('El número ' + num + ' no es divisible por 2.');
