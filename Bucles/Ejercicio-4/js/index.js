fe364084f91ea530655e4678319c24822bb7dd66
Sergio Moran <moransergio.01@gmail.com>
2020-12-15 20:25:49 -0300

    Bucles - Ejercicio 4

---
+++ Bucles/Ejercicio-4/js/index.js
@@ -0,0 +1,14 @@
+let userNum = 0;
+let accNums = 0;
+const msg = 'Ingresar otro número?';
+
+do
+{
+	userNum = parseFloat(prompt('Número:'));
+
+	if(userNum == NaN) alert('El dato no es un número.');
+	else accNums += userNum;
+
+}while(confirm(msg));
+
+alert('La suma total es: ' + accNums);
