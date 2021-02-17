75071fe8b4890939757a0c5f653dc46c9fb210f4
Sergio Morán <moransergio.01@gmail.com>
2021-02-16 21:38:14 -0300

    transicion en las agujas

--- DOM-BOM/Ejercicio-3/js/app.js
+++ DOM-BOM/Ejercicio-3/js/app.js
@@ -5,17 +5,14 @@ const getDate = ()=>
 		divMin = document.getElementById('min'),
 		divSec = document.getElementById('sec');
 
-	setInterval(()=>
-	{
-		let date = new Date(),
-			hs = date.getHours() * 30,
-			min = date.getMinutes() * deg,
-			sec = date.getSeconds() * deg;
+	let date = new Date(),
+		hs = date.getHours() * 30,
+		min = date.getMinutes() * deg,
+		sec = date.getSeconds() * deg;
 
-		divHs.style.transform = `rotateZ(${hs+(min/12)}deg)`;
-		divMin.style.transform = `rotateZ(${min}deg)`;
-		divSec.style.transform = `rotateZ(${sec}deg)`;
-	})
+	divHs.style.transform = `rotateZ(${hs+(min/12)}deg)`;
+	divMin.style.transform = `rotateZ(${min}deg)`;
+	divSec.style.transform = `rotateZ(${sec}deg)`;
 }
 
-getDate();
+setInterval(getDate, 1000);
