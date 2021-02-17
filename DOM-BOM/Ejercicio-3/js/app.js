0eb1e2a24e5731362bb3d840379835756c2215a8
Sergio Morán <75262494+slmoran90@users.noreply.github.com>
2021-02-16 23:06:46 -0300

    cometnarios en app.js

--- DOM-BOM/Ejercicio-3/js/app.js
+++ DOM-BOM/Ejercicio-3/js/app.js
@@ -1,15 +1,25 @@
 const getDate = ()=>
 {
+	/*360° equivale a una vuelta completa de reloj
+	por lo que 360/60 (minutos) es igual a 6°
+	por minuto*/
+
 	const deg = 6,
 		divHs = document.getElementById('hs'),
 		divMin = document.getElementById('min'),
 		divSec = document.getElementById('sec');
 
 	let date = new Date(),
+		/*las horas en el reloj estan separadas cada 5 minutos
+		por lo que la aguja recorre 30° entre hora y hora*/
 		hs = date.getHours() * 30,
+		/*multiplicar los minutos y segundos por la constante gradial
+		para poder posicionar la aguja en su correspondiente angulo*/
 		min = date.getMinutes() * deg,
 		sec = date.getSeconds() * deg;
 
+	/*las agujas giran por medio de la propiedad transform
+	solo asignandole sus grados correspondientes*/
 	divHs.style.transform = `rotateZ(${hs+(min/12)}deg)`;
 	divMin.style.transform = `rotateZ(${min}deg)`;
 	divSec.style.transform = `rotateZ(${sec}deg)`;
